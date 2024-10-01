b931f33def002f48f5c0fa3cce1b2c284fff220d
Gonza149 <elgonza149@gmail.com>
2024-10-01 00:19:44 -0300

    se sigue aprendiendo

--- GestorEventos/eventoapp/views.py
+++ GestorEventos/eventoapp/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1> Que onda broo</h1>')
+    
+    context = {
+        "nombre": "Francisco",
+        "edad": 44,
+        "nacionalidad": "Argentino"
+    }
+    return render(request, 'index.html', context)
