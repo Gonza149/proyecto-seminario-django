808f915220acd8607aa7ff48ad661957e1787143
Gonza149 <elgonza149@gmail.com>
2024-10-16 14:52:37 -0300

    Relaciones Evento-TipoEvento-Lugar

--- GestorEventos/eventoapp/admin.py
+++ GestorEventos/eventoapp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Feature
+from .models import Feature, Lugar, Evento, TipoEvento
 # Register your models here.
 
 admin.site.register(Feature)
+admin.site.register(Lugar)
+admin.site.register(Evento)
+admin.site.register(TipoEvento)
--- GestorEventos/eventoapp/models.py
+++ GestorEventos/eventoapp/models.py
@@ -5,5 +5,30 @@ from django.db import models
 class Feature(models.Model):
     nombre = models.CharField(max_length=20)
     descripcion = models.CharField(max_length=100)
-    
 
+    def __str__(self):
+        return f"{self.nombre}"    
+
+class Lugar(models.Model):
+    nombre  = models.CharField(max_length=50)
+    calle = models.CharField(max_length=50)
+    numero = models.IntegerField()
+    detalleLugar = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return f"{self.nombre}"
+
+class TipoEvento(models.Model):
+    nombre = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.nombre}"
+
+class Evento(models.Model):
+    nombre = models.CharField(max_length=100)
+    fechaEvento = models.DateField()
+    ubicacion = models.ForeignKey(Lugar, on_delete=models.CASCADE)
+    tipoEvento = models.ForeignKey(TipoEvento, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.ubicacion}"
