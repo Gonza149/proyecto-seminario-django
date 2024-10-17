95f243c2ac5f70d45c7026b4c16ba3c74c514aff
Gonza149 <elgonza149@gmail.com>
2024-10-17 16:52:56 -0300

    views

--- GestorEventos/eventoapp/views.py
+++ GestorEventos/eventoapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse 
-from .serializers import FeatureSerializer, TipoEventoSerializer
-from .models import Feature, TipoEvento
+from .serializers import FeatureSerializer, TipoEventoSerializer, LugarSerializer
+from .models import Feature, TipoEvento, Lugar
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -71,4 +71,12 @@ def listaTipoEventos(request, format=None):
 def listaLugares(request, format=None):
 
     if request.method == "GET":
-        pass
+        lugares = Lugar.objects.all()
+        serializer = LugarSerializer(lugares, many=True)
+        return Response(serializer.data)
+    
+    if request.method == "POST":
+        serializer = LugarSerializer(request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_201_CREATED)
